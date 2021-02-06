91e79a1084613019e32d53a63c1dd106365a58b0
Reymezis <qq1s@bk.ru>
2021-02-06 17:29:07 +0600

    fix readfile fn

--- src/parsers.php
+++ src/parsers.php
@@ -6,9 +6,12 @@ use Symfony\Component\Yaml\Yaml;
 
 function readFile(string $path): object
 {
+    if (!file_exists($path)) {
+        throw new \Exception("File '{$path}' unreadable or doesn't exist");
+    }
     $absolutePath = realpath($path);
     $fileFormat = pathinfo($path, PATHINFO_EXTENSION);
-    $data = (string) file_get_contents($absolutePath);
+    $data = file_get_contents($absolutePath);
     $parsersList = [
         "json" => json_decode($data),
         "yaml"  => Yaml::parse($data, Yaml::PARSE_OBJECT_FOR_MAP),
