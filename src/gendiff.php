8b903c607ef8e776e299645f297b7d5f8df3fbb5
Reymezis <qq1s@bk.ru>
2021-02-06 21:15:53 +0600

    refactor files parser

--- src/gendiff.php
+++ src/gendiff.php
@@ -4,6 +4,7 @@ namespace Differ\Differ;
 
 use function diff\astBuilder\buildAST;
 use function diff\Formatters\render;
+use function diff\parsers\parseData;
 use function diff\parsers\readFile;
 
 const DOC = <<<DOC
@@ -44,6 +45,12 @@ function genDiff($pathToFile1, $pathToFile2, $format = "stylish"): string
 {
     $data1 = readFile($pathToFile1);
     $data2 = readFile($pathToFile2);
-    $ast = buildAST($data1, $data2);
+
+    [$file1Format, $rawData1] = $data1;
+    [$file2Format, $rawData2] = $data2;
+
+    $parsedData1 = parseData($file1Format, $rawData1);
+    $parsedData2 = parseData($file2Format, $rawData2);
+    $ast = buildAST($parsedData1, $parsedData2);
     return render($ast, $format);
 }
--- src/parsers.php
+++ src/parsers.php
@@ -4,7 +4,7 @@ namespace diff\parsers;
 
 use Symfony\Component\Yaml\Yaml;
 
-function readFile(string $path): object
+function readFile(string $path): array
 {
     if (!file_exists($path)) {
         throw new \Exception("File '{$path}' unreadable or doesn't exist");
@@ -12,9 +12,18 @@ function readFile(string $path): object
     $absolutePath = realpath($path);
     $fileFormat = pathinfo($path, PATHINFO_EXTENSION);
     $data = file_get_contents($absolutePath);
-    $parsersList = [
-        "json" => json_decode($data),
-        "yaml"  => Yaml::parse($data, Yaml::PARSE_OBJECT_FOR_MAP),
-    ];
-    return $parsersList[$fileFormat];
+    return [$fileFormat, $data];
+}
+
+function parseData($fileFormat, $data): object
+{
+    switch ($fileFormat) {
+        case 'yml':
+        case 'yaml':
+            return Yaml::parse($data, Yaml::PARSE_OBJECT_FOR_MAP);
+        case 'json':
+            return json_decode($data);
+        default:
+            throw new \Exception("Unknown format {$fileFormat}");
+    }
 }
